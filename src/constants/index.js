35f1cd80dfe43e8438ead3f8d6c40da2bdbd2df6
sathya6862 <sathyasai.8225@gmail.com>
2025-10-11 23:20:25 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,329 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "E-commerce Platform",
+    description:
+      "Facilitates purchases from international websites like Amazon and eBay, allowing customers to shop from these sites and have products delivered domestically.",
+    subDescription: [
+      "Built a scalable application with ASP.NET Core MVC, integrating global platforms like Amazon for domestic delivery.",
+      "Implemented secure authentication and database management using ASP.NET Core Identity and Entity Framework Core.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added payment systems, localization, and product filtering for functionality improvements.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/accessories.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 2,
+        name: ".Net",
+        path: "/assets/logos/dotnet.svg",
+      },
+      {
+        id: 3,
+        name: "Ef Core",
+        path: "/assets/logos/efcore.png",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Event page",
+    description:
+      "Developed a responsive event webpage for “Strangers”, a national-level event organized by the Rotaract Club of Chennai..",
+    subDescription: [
+      "Built the frontend using React.js and Tailwind CSS, ensuring a modern and fluid user interface.",
+      "Designed a unique bottom navigation bar, offering a creative alternative to traditional top nav layouts.",
+      "Developed a React-based frontend with Tailwind CSS for a sleek user experience.",
+      "mplemented smooth transitions, dynamic content rendering, and responsive design for an engaging user experience.",
+      "Collaborated with a team to plan, design, and deploy the application efficiently.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/rotaract.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "SQLite",
+        path: "/assets/logos/threejs.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Job Portal Website",
+    description:
+      "A dynamic and responsive job portal built during my internship at Codec Technologies, designed to streamline job search and recruitment processes.",
+    subDescription: [
+      "Developed a fully functional web application using React.js and JavaScript, delivering a seamless Single Page Application (SPA) experience.",
+      "Implemented smooth animations and interactive UI components with Tailwind CSS for an engaging user interface..",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+      "Connected the frontend and backend using RESTful APIs for efficient data exchange",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/jobportal.webp",
+    tags: [
+      {
+        id: 1,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongodb-svgrepo-com.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Event Management Tool",
+    description:
+      "A lightweight C++ game engine designed for 2D and 3D game development.",
+    subDescription: [
+      "Built with MERN stack (MongoDB, Express.js, React.js, Node.js).",
+      "Designed a clean, responsive UI using React and Tailwind CSS",
+      "Integrated MongoDB for efficient event and user data handling.",
+      "Added smooth minimal transitions for an elegant user experience.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/Eventmanagement.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 4,
+        name: "MongoDB",
+        path: "/assets/logos/mongodb-svgrepo-com.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "WordPress Custom Theme",
+    description:
+      "A fully customizable WordPress theme optimized for performance and SEO.",
+    subDescription: [
+      "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Optimized SEO and page speed using Vite.js for fast builds.",
+      "Implemented custom widgets and plugin compatibility for extended functionality.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/wordpress-theme.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "WordPress",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "Vite.js",
+        path: "/assets/logos/vitejs.svg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Online Learning Platform",
+    description:
+      "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+    subDescription: [
+      "Built using Blazor WebAssembly for a seamless SPA experience.",
+      "Implemented video streaming with Azure Media Services.",
+      "Added a quiz system with dynamic question generation and real-time grading.",
+      "Integrated Stripe API for secure payment processing.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/elearning.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Blazor",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: "Azure",
+        path: "/assets/logos/azure.svg",
+      },
+      {
+        id: 3,
+        name: "Stripe",
+        path: "/assets/logos/stripe.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "Github",
+    href: "https://github.com/Sathya6862",
+    icon: "/assets/socials/github-opened-svgrepo-com.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/sathyasaikumar/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/sathya__6862?igsh=NDJoYnZ5dHo4dmZj",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "First Step in IT",
+    job: "Frontend Developer Intern",
+    date: "2023-2024",
+    contents: [
+      "Starting my web development journey:",
+      "1️⃣ Started my web development journey by learning the basics of HTML, CSS, and JavaScript, which sparked my interest in creating websites and applications.",
+      "2️⃣ Completed the Full-Stack Development course at GUVI, building a solid foundation in both front-end and back-end development.",
+      "3️⃣ Gained practical experience as a Full-Stack Developer Intern at Skill Forge, where I built a fully responsive login page using multiple technologies",
+      "4️⃣ Enhanced my skills further at Codec Technologies by developing a Job Portal Website and a Project Management Tool with complete front-end and back-end functionality.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    job: "Intern",
+    date: "2024-2025",
+    contents: [
+      "Built projects to strengthen my skills in web development and full-stack technologies:",
+      "✅ Completed Full-Stack Development course at GUVI, gaining hands-on experience with front-end and back-end tools.",
+      "✅ At Codec Technologies, I developed a job portal website and a project management tool with both front-end and back-end functionality.",
+      "✅ Learning the basics of REST APIs and how databases store and manage data.",
+      "✅ Curious about new tools, frameworks, and technologies in web development.",
+    ],
+  },
+  {
+    title: "Mern Stack Developer",
+    job: "Freelancer",
+    date: "Present",
+    contents: [
+      "✨ Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "🚀 Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Lakshmipathy",
+    username: "@lacha123",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Roshni",
+    username: "@roshhhhh",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "Harish",
+    username: "@harish",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Priya dharshini",
+    username: "@priyadharshini",
+    body: "The website looks amazing and works flawlessly.",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Anand kumar",
+    username: "@anandkumar",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Keerthivasan",
+    username: "@keerthivasan",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Shobann kumar",
+    username: "@shobannkumar",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Naveen kumar",
+    username: "@naveenkumar",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
